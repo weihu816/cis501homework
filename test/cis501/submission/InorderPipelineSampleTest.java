2514290342c5abf8c0a97ce71b76e9be714defab
Joe Devietti <devietti@cis.upenn.edu>
2016-09-14 12:27:01 -0400

    HW3 sample tests

---
+++ test/cis501/submission/InorderPipelineSampleTest.java
@@ -0,0 +1,132 @@
+package cis501.submission;
+
+import cis501.Bypass;
+import cis501.IInorderPipeline;
+import cis501.Insn;
+import cis501.MemoryOp;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class InorderPipelineSampleTest {
+
+    private static IInorderPipeline sim;
+
+    private static Insn makeInsn(int dst, int src1, int src2, MemoryOp mop) {
+        return new Insn(dst, src1, src2,
+                1, 4,
+                null, 0, null,
+                mop, 1, 1,
+                "synthetic");
+    }
+
+    @Before
+    public void setup() {
+        sim = new InorderPipeline(0/*no add'l memory latency*/, Bypass.FULL_BYPASS);
+    }
+
+    @Test
+    public void test1Uop() {
+        List<Insn> insns = new LinkedList<>();
+        insns.add(makeInsn(3, 1, 2, null));
+        sim.run(insns);
+        assertEquals(1, sim.getInsns());
+        // 12345678
+        // fdxmw|
+        assertEquals(6, sim.getCycles());
+    }
+
+    @Test
+    public void testManyUops() {
+        List<Insn> insns = new LinkedList<>();
+        final int COUNT = 10;
+        for (int i = 0; i < COUNT; i++) {
+            insns.add(makeInsn(3, 1, 2, null));
+        }
+        sim.run(insns);
+        assertEquals(COUNT, sim.getInsns());
+        assertEquals(5 + COUNT, sim.getCycles());
+    }
+
+    @Test
+    public void test1MemUop() {
+        List<Insn> insns = new LinkedList<>();
+        insns.add(makeInsn(3, 1, 2, MemoryOp.Load));
+        sim.run(insns);
+        assertEquals(1, sim.getInsns());
+        // 123456789abcdef
+        // fdxmw|
+        assertEquals(6, sim.getCycles());
+    }
+
+    @Test
+    public void testManyMemUops() {
+        List<Insn> insns = new LinkedList<>();
+        final int COUNT = 10;
+        for (int i = 0; i < COUNT; i++) {
+            insns.add(makeInsn(3, 1, 2, MemoryOp.Store)); // no load-use dependencies
+        }
+        sim.run(insns);
+        assertEquals(COUNT, sim.getInsns());
+        assertEquals(5 + COUNT, sim.getCycles());
+    }
+
+    @Test
+    public void testLoadUse1() {
+        List<Insn> insns = new LinkedList<>();
+        insns.add(makeInsn(3, 1, 2, MemoryOp.Load));
+        insns.add(makeInsn(5, 3, 4, null)); // load to src reg 1
+        sim.run(insns);
+        assertEquals(2, sim.getInsns());
+        // 123456789abcdef
+        // fdxmw  |
+        //  fd.xmw|
+        assertEquals(6 + 2, sim.getCycles());
+    }
+
+    @Test
+    public void testLoadUse2() {
+        List<Insn> insns = new LinkedList<>();
+        insns.add(makeInsn(3, 1, 2, MemoryOp.Load));
+        insns.add(makeInsn(5, 4, 3, null)); // load to src reg 2
+        sim.run(insns);
+        assertEquals(2, sim.getInsns());
+        // 123456789abcdef
+        // fdxmw  |
+        //  fd.xmw|
+        assertEquals(6 + 2, sim.getCycles());
+    }
+
+    @Test
+    public void testLoadUseStoreAddress() {
+        List<Insn> insns = new LinkedList<>();
+        insns.add(makeInsn(3, 1, 2, MemoryOp.Load));
+        insns.add(makeInsn(5, 4, 3, MemoryOp.Store)); // load to src reg 2 (store address), so we stall
+        sim.run(insns);
+        assertEquals(2, sim.getInsns());
+        // 123456789abc
+        // fdxmw  |
+        //  fd.xmw|
+        final long expected = 6 + 2;
+        assertEquals(expected, sim.getCycles());
+    }
+
+    @Test
+    public void testLoadUseStoreValue() {
+        List<Insn> insns = new LinkedList<>();
+        insns.add(makeInsn(3, 1, 2, MemoryOp.Load));
+        insns.add(makeInsn(5, 3, 4, MemoryOp.Store)); // load to src reg 1 (store value), so no stall
+        sim.run(insns);
+        assertEquals(2, sim.getInsns());
+        // 123456789abcdef
+        // fdxmw |
+        //  fdxmw|
+        final long expected = 6 + 1;
+        assertEquals(expected, sim.getCycles());
+    }
+
+}
