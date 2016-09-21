5cbd07830cda000e3550638777d9a7917fc33cce
Wei <huwei@seas.upenn.edu>
2016-09-21 14:14:21 -0400

    Add initial commit for hw1 (pass all current test)

--- src/cis501/submission/InorderPipeline.java
+++ src/cis501/submission/InorderPipeline.java
@@ -3,6 +3,8 @@ package cis501.submission;
 
 import cis501.*;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -12,6 +14,7 @@ import java.util.Set;
 enum Stage {
     WRITEBACK(4), MEMORY(3), EXECUTE(2), DECODE(1), FETCH(0);
 
+    public static final int NUM_STAGES = 5;
     private final int index;
 
     private Stage(int idx) {
@@ -22,9 +25,22 @@ enum Stage {
     public int i() {
         return index;
     }
+
+    public boolean toLeave() {
+        return index == NUM_STAGES - 1;
+    }
 }
 
 public class InorderPipeline implements IInorderPipeline {
+
+    /* Five stages herer: F D X M W */
+    private Insn[] latches = new Insn[Stage.NUM_STAGES];
+    /* Given Parameters */
+    private int additionalMemLatency;
+    private Set<Bypass> bypasses;
+    /* Running Statics */
+    private int insnCounter = 0;
+    private int cycleCounter = 0;
 
     /**
      * Create a new pipeline with the given additional memory latency.
@@ -37,26 +53,85 @@ public class InorderPipeline implements IInorderPipeline {
      *                             stalling to resolve all data hazards.
      */
     public InorderPipeline(int additionalMemLatency, Set<Bypass> bypasses) {
-
+        this.additionalMemLatency = additionalMemLatency;
+        this.bypasses = new HashSet<>(bypasses);
     }
 
     @Override
     public String[] groupMembers() {
-        return new String[]{"your", "names"};
+        return new String[]{"Wei Hu", "Dongni Wang"};
     }
 
     @Override
     public void run(Iterable<Insn> ii) {
-
+        Iterator<Insn> insnIterator = ii.iterator();
+        while (insnIterator.hasNext() || !isEmpty()) {
+            advance(insnIterator);
+            cycleCounter++;
+        }
     }
 
+    // ----------------------------------------------------------------------
+    // Pipeline Stage Latches Operations
+    // ----------------------------------------------------------------------
     @Override
     public long getInsns() {
-        return 0;
+        return insnCounter;
     }
 
     @Override
     public long getCycles() {
-        return 0;
+        return cycleCounter;
+    }
+
+    public Insn getInsn(Stage stage) {
+        return latches[stage.i()];
+    }
+
+    public void clear(Stage stage) {
+        latches[stage.i()] = null;
+    }
+
+    public boolean isEmpty() {
+        for (Insn insn : latches)
+            if (insn != null) return false;
+        return true;
+    }
+
+    private void advance(Stage stage) {
+        if (!stage.toLeave()) {
+            latches[stage.i()+1] = latches[stage.i()];
+        }
+        clear(stage);
+    }
+
+    private void fetchInsn(Insn insn) {
+        latches[Stage.FETCH.i()] = insn;
+        insnCounter++;
+    }
+
+    public void advance(Iterator<Insn> iterator) {
+        // Current stages snapshot
+        Insn wi = getInsn(Stage.WRITEBACK);
+        Insn mi = getInsn(Stage.MEMORY);
+        Insn xi = getInsn(Stage.EXECUTE);
+        Insn di = getInsn(Stage.DECODE);
+        Insn fi = getInsn(Stage.FETCH);
+        // Advance the pipeline in the reverse order
+        advance(Stage.WRITEBACK);
+        advance(Stage.MEMORY);
+        advance(Stage.EXECUTE);
+        // Stall on Load-To-Use Dependence
+        if (stallOnLoadToUseDependence(di, xi)) { return; }
+        advance(Stage.DECODE);
+        advance(Stage.FETCH);
+        if (iterator.hasNext()) { fetchInsn(iterator.next()); }
+        else { clear(Stage.FETCH); }
+    }
+
+    private boolean stallOnLoadToUseDependence(Insn di, Insn xi) {
+        if (di == null || xi == null) return false;
+        return (xi.mem == MemoryOp.Load) && ( (di.srcReg2 == xi.dstReg) ||
+                ((di.srcReg1 == xi.dstReg) && (di.mem != MemoryOp.Store)));
     }
 }
