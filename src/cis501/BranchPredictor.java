4c4f707dcd4ae12b3c915fc1ece7aa3b1d8f1d5a
Joe Devietti <devietti@cis.upenn.edu>
2016-09-27 21:42:56 -0400

    use PC>>1 to index predictors

--- src/cis501/BranchPredictor.java
+++ src/cis501/BranchPredictor.java
@@ -13,16 +13,19 @@ public class BranchPredictor {
 
     /**
      * @param pc            the PC of an insn i (which may or may not be a branch) on which we make
-     *                      our prediction
+     *                      our prediction. For ARM, this is the actual PC of the insn; the caller
+     *                      does not need to discard the LSB.
      * @param fallthroughPC the fallthrough PC of insn i, computed from i's pc and size
      * @return the predicted next PC
      */
     public long predict(long pc, long fallthroughPC) {
-        long btbTarget = btb.predict(pc);
+        assert 0 == (pc & 0x1) : "lsb of PC should always be 0 for ARM";
+        final long predictorIndex = pc >> 1;
+        long btbTarget = btb.predict(predictorIndex);
         // if it's not a (taken) branch, just predict fallthroughPC
         if (0 == btbTarget) return fallthroughPC;
 
-        Direction d = dp.predict(pc);
+        Direction d = dp.predict(predictorIndex);
         switch (d) {
             case Taken:
                 return btbTarget;
@@ -37,16 +40,20 @@ public class BranchPredictor {
     /**
      * Update the predictor state based on the actual behavior of the branch.
      *
-     * @param pc           the program counter of the branch to train
+     * @param pc           The program counter of the branch to train. For ARM, this is the actual
+     *                     PC of the insn; the caller does not need to discard the LSB.
      * @param actualNextPC the true next PC following the branch
      * @param actualDir    the actual direction of the branch
      */
     public void train(long pc, long actualNextPC, Direction actualDir) {
+        assert 0 == (pc & 0x1) : "lsb of PC should always be 0 for ARM";
+        final long predictorIndex = pc >> 1;
         // only record taken branches in the BTB
         if (Direction.Taken == actualDir) {
-            btb.train(pc, actualNextPC);
+            btb.train(predictorIndex, actualNextPC);
         }
-        dp.train(pc, actualDir);
+        // train direction predictor no matter the direction
+        dp.train(predictorIndex, actualDir);
     }
 
     @Override
