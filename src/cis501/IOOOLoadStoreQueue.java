5b9efe35424d2fb600e75bc6dc6580dd5c996190
Joseph Devietti <devietti@users.noreply.github.com>
2016-11-27 22:35:09 -0500

    load/store values are actually big-endian

--- src/cis501/IOOOLoadStoreQueue.java
+++ src/cis501/IOOOLoadStoreQueue.java
@@ -39,8 +39,8 @@ public interface IOOOLoadStoreQueue {
      * @param handle  identifier for the load that is executing
      * @param address The data address of the load, always an aligned address.
      * @return the value returned by the load. For multi-byte loads, this value will be in
-     * little-endian byte order. E.g., if memory addresses 0-3 contain values 0x00, 0x11, 0x22,
-     * 0x33, respectively, then a 4-byte load from address 0x0 will return 0x00112233
+     * big-endian byte order. E.g., if memory addresses 0-3 contain values 0x11, 0x22, 0x33,
+     * 0x44, respectively, then a 4-byte load from address 0x0 will return 0x11223344
      */
     long executeLoad(LoadHandle handle, long address);
 
@@ -50,7 +50,7 @@ public interface IOOOLoadStoreQueue {
      * @param handle  identifier for the store that is executing
      * @param address The data address of the store, always an aligned address.
      * @param value   The value being written. For multi-byte stores, this value should be in
-     *                little-endian byte order. E.g., if you want to write to memory addresses 0-3
+     *                big-endian byte order. E.g., if you want to write to memory addresses 0-3
      *                with values 0x11, 0x22, 0x33, 0x44 respectively, then this parameter should be
      *                0x11223344
      * @return the loads that are now known to be mis-speculated based on this store instruction
